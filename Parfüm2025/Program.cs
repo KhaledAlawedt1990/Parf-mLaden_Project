00285ab8d7f2fe99b34dc522b9e3fe63fbb37633
Khaled Alawedat <khaled.alawedat90@gmail.com>
2024-12-17 13:39:21 +0100

    Aktualisiert Am 17.12.2024

--- Parfüm2025/Program.cs
+++ Parfüm2025/Program.cs
@@ -16,11 +16,11 @@ namespace Parfüm2025
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-           // Application.Run(new frmpafümAnsicht());
+           Application.Run(new frmpafümAnsicht());
        
            // Application.Run(new frmMitarbeiterListe());
        
-              Application.Run(new frmLogginScreen());
+              //Application.Run(new frmLogginScreen());
             // Application.Run(new frmKundeListe());
             //Application.Run(new frmMainScreen());
             //Application.Run(new frmUserListe());
